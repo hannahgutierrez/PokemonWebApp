8ccc58767a549f7aaae644ee5ce6f99d3c0bf830
Hannah Gutierrez <hannah_gutierrez@dlsu.edu.ph>
2026-07-16 21:44:48 +0800

    Added constructor in Pokemon

--- src/main/java/ph/edu/dlsu/lbycpob/pokemonwebapp/model/Pokemon.java
+++ src/main/java/ph/edu/dlsu/lbycpob/pokemonwebapp/model/Pokemon.java
@@ -1,4 +1,9 @@
 package ph.edu.dlsu.lbycpob.pokemonwebapp.model;
 
 public class Pokemon extends AbstractPokemon{
+    public Pokemon(int instanceId, String name, double weight, double height,
+                   double attack, double defense, double stamina, String type) {
+        super(instanceId, name, weight, height, attack, defense, stamina, type);
+    }
+
 }
