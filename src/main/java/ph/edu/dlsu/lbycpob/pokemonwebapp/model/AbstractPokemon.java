efe88bc6c78573ef7389ee98955c97cc5c1a1e4a
Hannah Gutierrez <hannah_gutierrez@dlsu.edu.ph>
2026-07-16 21:40:01 +0800

    Added construction in AbstractPokemon

--- src/main/java/ph/edu/dlsu/lbycpob/pokemonwebapp/model/AbstractPokemon.java
+++ src/main/java/ph/edu/dlsu/lbycpob/pokemonwebapp/model/AbstractPokemon.java
@@ -10,4 +10,16 @@ public abstract class AbstractPokemon implements PokemonOperations{
     protected double stamina;
     protected String type;
 
+    public AbstractPokemon(int instanceId, String name, double weight, double height,
+                           double attack, double defense, double stamina, String type) {
+        this.instanceId = instanceId;
+        this.name = name;
+        this.weight = weight;
+        this.height = height;
+        this.attack = attack;
+        this.defense = defense;
+        this.stamina = stamina;
+        this.type = type;
+    }
+
 }
